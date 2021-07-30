a55a5ce50ec0c259868a01145c42fdc890c3bd03
Athos Damiani <athos.damiani@gmail.com>
2021-07-30 15:39:09 -0300

    asd

--- scripts/01-visao.R
+++ scripts/01-visao.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(broom)
 visao <- read_csv2("https://raw.githubusercontent.com/curso-r/main-regressao-linear/master/misc/visao.csv")
 
+ggplot(visao) + geom_point(aes(idade, distancia))
+
 # regressao
 modelo <- lm(distancia ~ idade, data = visao)
 
